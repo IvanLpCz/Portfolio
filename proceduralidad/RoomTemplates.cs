70d5c34a3843c3e1db053eb734e64d4bb4cadcdb
IvanLpCz <83703298+IvanLpCz@users.noreply.github.com>
2022-01-10 16:21:12 +0100

    Add files via upload

---
+++ proceduralidad/RoomTemplates.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour {
+
+	public GameObject[] bottomRooms;
+	public GameObject[] topRooms;
+	public GameObject[] leftRooms;
+	public GameObject[] rightRooms;
+
+	public GameObject closedRoom;
+
+	public List<GameObject> rooms;
+
+	//public float waitTime;
+	//private bool spawnedBoss;
+	public GameObject boss;
+	public GameObject enemies;
+
+    private void Start()
+    {
+        Invoke("SpawnEnemies", 2f);
+    }
+
+    void SpawnEnemies()
+    {
+        Instantiate(boss, rooms[rooms.Count - 1].transform.position, Quaternion.identity);
+
+    }
+}
